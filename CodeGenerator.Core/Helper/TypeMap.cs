ebc937dfcb5d8d188f279ce1ef5d85a4b8fe18ec
MarvelTiter <outlinmt@outlook.com>
2021-12-31 11:32:44 +0800

    备份

--- CodeGenerator.Core/Helper/TypeMap.cs
+++ CodeGenerator.Core/Helper/TypeMap.cs
@@ -9,13 +9,33 @@ namespace CodeGenerator.Core.Helper
 {
     public class TypeMap
     {
+       
         public static string Map(string dbType, string nullable)
         {
-            return "";
+            /*
+             * 临时硬编码
+             */
+            var d = dbType.ToLower();
+            if (d.Contains("char"))
+            {
+                return "string";
+            }
+            else if (d.Contains("int") || d.Contains("bit"))
+            {
+                return nullable == "YES" ? "int?" : "int";
+            }
+            else if (d.Contains("date"))
+            {
+                return nullable == "YES" ? "DateTime?" : "DateTime";
+            }
+            else
+            {
+                throw new Exception($"未知数据类型 {dbType}");
+            }
         }
         public TypeMap()
         {
-            
+
         }
     }
 }
